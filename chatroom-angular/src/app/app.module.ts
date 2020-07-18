05ddbe739e7e527bb14544e229fa4cdb856606f8
Paulo Jorge <mfjpaulo@gmail.com>
2020-07-18 21:39:42 +0100

    Built basic file structure layout w/ new favicon and loader

--- chatroom-angular/src/app/app.module.ts
+++ chatroom-angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChatroomComponent } from './shared/components/chatroom/chatroom.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChatroomComponent
   ],
   imports: [
     BrowserModule,
