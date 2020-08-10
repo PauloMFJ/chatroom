3f51e404ba104e7a715611e2c84b8ac6af6bcc60
Paulo Jorge <mfjpaulo@gmail.com>
2020-08-10 16:57:29 +0100

    Updated production serverApi

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 
 export const environment = {
   production: false,
-  serverApi: 'http://localhost:5000/',
+  serverApi: 'https://app-chatroom-api.herokuapp.com/',
   portfolioWebsiteBase: 'http://www.paulojorge.co.uk/',
 };
