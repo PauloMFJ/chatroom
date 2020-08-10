28bd77ffe75da2209e6f0fa1b1a1b8eb401f0117
Paulo Jorge <mfjpaulo@gmail.com>
2020-08-10 22:04:20 +0100

    Updated watermark responsiveness and implemented new github watermark

--- src/app/shared/components/watermark/watermark.component.ts
+++ src/app/shared/components/watermark/watermark.component.ts
@@ -8,6 +8,7 @@ import { environment } from 'src/environments/environment';
 })
 export class WatermarkComponent {
 
+  pathToGithub = `${environment.githubWebsiteBase}`;
   pathToPortfolio = `${environment.portfolioWebsiteBase}`;
 
   constructor() { }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,4 +3,5 @@ export const environment = {
   production: false,
   serverApi: 'http://localhost:5000/',
   portfolioWebsiteBase: 'http://www.paulojorge.co.uk/',
+  githubWebsiteBase: 'https://github.com/PauloMFJ/'
 };
